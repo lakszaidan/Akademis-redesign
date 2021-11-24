f01f9c33bfd7fe084ec701496faebccdfc494c20
Laksamana Zaidan <67093110+lakszaidan@users.noreply.github.com>
2021-11-24 07:43:42 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        nav_bg: '#E7F2F6',
+        nav_btn: '#60C1E7',
+        step_num: '#BEEBFF',
+        text: {
+          h1: '#1F78B4',
+          nav: '#152C5B'
+        }
+      },
+      fontFamily: {
+        body: ['Poppins']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
